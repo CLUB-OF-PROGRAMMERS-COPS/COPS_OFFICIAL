198ab93f58109e9f3afa7a37135812b8c6355f6a
DEEPAK P S <154110248+DeepakKrishna-DK@users.noreply.github.com>
2026-03-18 12:15:45 +0530

    Change eventsConfirmed to true

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -4,7 +4,7 @@ import { ImageWithFallback } from "@/app/components/figma/ImageWithFallback";
 
 export const Events = () => {
   // Set this to true and fill in event details when events are confirmed
-  const eventsConfirmed = false;
+  const eventsConfirmed = true;
   ///viewform?embedded=true(to embed google form in iframe, add ?embedded=true at the end of form URL)
   const events = [
     {
